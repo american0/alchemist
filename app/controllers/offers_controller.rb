23ac9dbb6156955bad7578c2f7c12d3eb28d444f
Paul Chavinier <paul.chavinier@gmail.com>
2016-05-17 12:31:43 +0200

    offers controller + view added

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,25 @@
 class OffersController < ApplicationController
+
+  def new
+    @offer = Offer.new
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    @offer = Offern.new(offer_params)
+    @user = User.find(params[:user_id])
+    @offer.user = @user
+    if @offer.save
+      redirect_to user_path(@user)
+    else
+      render 'offers/new'
+    end
+  end
+
+
+  def offer_params
+    params.require(:offer).permit(:title, :description, :base_rice, :price_20, :price_50)
+  end
+
+
 end

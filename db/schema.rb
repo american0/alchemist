72e43ef990292fb90b65be8b0f91fe1992535aef
Constant Desfosse <desfosse.constant@gmail.com>
2016-05-17 16:13:40 +0200

    login ok

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516160936) do
+ActiveRecord::Schema.define(version: 20160517104117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,11 @@ ActiveRecord::Schema.define(version: 20160516160936) do
     t.string   "last_name"
     t.text     "description"
     t.string   "address"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "picture"
+    t.string   "token"
+    t.datetime "token_expiry"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

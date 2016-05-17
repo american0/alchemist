95c0e0dd3fee12389210542507b35d47da4a3f83
Fernando Americano <americano@gmail.com>
2016-05-17 16:57:56 +0200

    offer and booking work in progress

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,29 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(bookign_params)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def bookign_params
+    params.require(:booking).permit(:date, :address)
+  end
+end

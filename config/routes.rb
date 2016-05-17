5a3853d910560678ad443aa8fc3211002f7efe64
Fernando Americano <americano@gmail.com>
2016-05-17 16:36:17 +0200

    new router

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
 
   resources :cocktails
   resources :reviews
-  resources :offers
+  resources :offers do
+    resources :bookings
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

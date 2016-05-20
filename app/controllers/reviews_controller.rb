5b0419b6fc5945b1b7b0a37cd14c74ee18bc1c38
Fernando Americano <americano@gmail.com>
2016-05-20 15:13:26 +0200

    wip

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,10 @@
 class ReviewsController < ApplicationController
+  before_action :find_booking
+
+
+  private
+
+  def find_booking
+    @booking = Booking.find(params[:booking_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,10 @@ Rails.application.routes.draw do
 
 
   resources :cocktails
-  resources :reviews
   resources :offers do
-    resources :bookings
+    resources :bookings do
+      resources :reviews
+    end
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
